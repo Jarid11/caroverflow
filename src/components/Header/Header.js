691ab56e54abe58b0ff7e7b208aa9ccfec5a5831
Jarid11 <Marinosj1995@gmail.com>
2018-08-14 16:52:42 -0500

    added hamburger btn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Header.css";
 
-export default function Header() {
-  return (
-    <header>
-      <div className="hamburger-container">
-        <div className="bar bar1" />
-        <div className="bar bar2" />
-        <div className="bar bar3" />
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: ["Home", "Tags", "Users", "Jobs"],
+      isChecked: false
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      isChecked: !this.state.isChecked
+    });
+  };
+
+  render() {
+    let { categories, isChecked } = this.state;
+    console.log(isChecked);
+    return (
+      <div id="menuToggle">
+        <input
+          type="checkbox"
+          onClick={this.toggleMenu}
+          checked={isChecked && true}
+        />
+
+        <span />
+        <span />
+        <span />
+
+        <ul id="menu" onChange={this.toggleMenu}>
+          {categories.map((e, i) => {
+            return (
+              <a key={i} href="#" onClick={this.toggleMenu} className="links">
+                {e}
+              </a>
+            );
+          })}
+        </ul>
       </div>
-    </header>
-  );
+    );
+  }
 }
+
+export default Header;
+
+// <header>
+//   <div className="hamburger-container" onClick={this.handleHamburgerMenu}>
+//     <div className="bar bar1" />
+//     <div className="bar bar2" />
+//     <div className="bar bar3" />
+//   </div>
+// </header>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./Home.css";
 import Header from "../Header/Header";
 
 export default function Home() {
   return (
-    <main>
+    <main className="home-container">
       <Header />
     </main>
   );
